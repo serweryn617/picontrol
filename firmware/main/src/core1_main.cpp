b0e46d87c32c04dee742270ac713cafce3ddfd46
serweryn617 <seweryn.r.1999@gmail.com>
2025-11-30 20:41:35 +0100

    update queue

--- firmware/main/src/core1_main.cpp
+++ firmware/main/src/core1_main.cpp
@@ -1,9 +1,25 @@
 #include "queue/queue.hpp"
 #include "main/core1_main.hpp"
 
+using namespace lib::queue;
+
 void core1_main()
 {
     while(true) {
-        // uint8_t command = g_queue_to_core1.get();
+        CoreCommand cmd{};
+        bool getOk = g_queue_to_core1.get(cmd);
+        if (!getOk) {
+            continue;
+        }
+
+        uint32_t number{};
+        switch (cmd.command_type) {
+
+            case CoreCommandType::square_number:
+                number = cmd.payload[0];
+                break;
+            case CoreCommandType::div_by_2_number:
+                break;
+        }
     }
 }
