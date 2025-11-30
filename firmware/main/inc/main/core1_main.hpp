e4e64938077564b6cfaaf9031c8820019e3269c2
serweryn617 <seweryn.r.1999@gmail.com>
2025-11-30 11:20:42 +0100

    launch core1 and add queue

---
+++ firmware/main/inc/main/core1_main.hpp
@@ -0,0 +1,6 @@
+#ifndef MAIN_CORE1_MAIN_HPP
+#define MAIN_CORE1_MAIN_HPP
+
+void core1_main();
+
+#endif  // MAIN_CORE1_MAIN_HPP
---
+++ firmware/main/src/core1_main.cpp
@@ -0,0 +1,9 @@
+#include "queue/queue.hpp"
+#include "main/core1_main.hpp"
+
+void core1_main()
+{
+    while(true) {
+        // uint8_t command = g_queue_to_core1.get();
+    }
+}
